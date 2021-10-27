ac83ccfe38a3eaafd09f2c9ff459453b3b9d881d
Davi Sousa <davi.gomes.sousa@ccc.ufcg.edu.br>
2021-10-27 10:54:20 -0300

    feature: add vertex shader

--- StartingOpenGL/src/main.cpp
+++ StartingOpenGL/src/main.cpp
@@ -2,6 +2,48 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
+// Our vertex shader source code in GLSL (OpenGL Shading Language)
+// Just foward all vertex coordinates to its vec4
+const char* vertexShaderSource = "#version 330 core\n"
+	"layout (location = 0) in vec3 aPos;\n"
+	"void main()\n"
+	"{\n"
+	"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+	"}\0";
+
+void buildVertexShader() {
+	// Define triangle vertices in NDC (Normalized Device Coordinates) (Vertex Data)
+	float vertices[] = {
+		-0.5f, -0.5f, 0.0f,
+		 0.5f, -0.5f, 0.0f,
+		 0.0f,  0.5f, 0.0f
+	};
+
+	// Create a vertex buffer object
+	unsigned int VBO;
+	// Generate its ID
+	glGenBuffers(1, &VBO);
+	// Bind VBO to a buffer type
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	// Copy vertex data into buffer's memory
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	// Create vertex shader with its source code
+	unsigned int vertexShader;
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+
+	// Check if vertex shader compilation failed
+	int success;
+	char infoLog[512];
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+	if (!success) {
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+}
+
 // Callback to resize openGL viewport as the window
 void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
 	glViewport(0, 0, width, height);
@@ -40,19 +82,22 @@ int main() {
 		return -1;
 	}
 
+	buildVertexShader();
+
 	// Set openGL viewport and ensures resizing
 	glViewport(0, 0, 800, 600);
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-	// GUI main loop, while not should be closed
+	// render loop, while not should be closed
 	while (!glfwWindowShouldClose(window)) {
 		// Process input
 		processInput(window);
 
-		// Rendering commands
+		// ----- Rendering commands -----
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
+		// ----------
 		// Swap window buffers and call events if needed
 		glfwSwapBuffers(window);
 		glfwPollEvents();
